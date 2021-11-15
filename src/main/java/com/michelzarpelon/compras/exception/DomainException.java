1e0c6bd3a51515f6005243701ee04e8261cc2517
Michel Zarpelon <michel.zarpelon@exacta.com.br>
2021-11-15 19:51:54 -0300

    customizando error

---
+++ src/main/java/com/michelzarpelon/compras/exception/DomainException.java
@@ -0,0 +1,9 @@
+package com.michelzarpelon.compras.exception;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
